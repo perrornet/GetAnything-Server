1db2a8c06b03946d25f67bae3c64a8a47e15af2e
perror <15058342792@163.com>
2019-01-08 18:20:41 +0800

    添加自定义错误类型

---
+++ error/error.go
@@ -0,0 +1,12 @@
+package error
+
+var (
+	UrlError             = &newError{msg: "请检查url是否正确！"}
+	NotFoundHandlerError = &newError{msg: "没有寻找到能够处理当前url的程序，我们已经记录该url，将会尽快提供支持。"}
+	ClientError          = &newError{msg: "客户端发送请求出现错误，我们将尽快解决！"}
+	ServerError          = &newError{msg: "服务器出现错误，我们将尽快解决！"}
+)
+
+type newError struct{ msg string }
+
+func (u *newError) Error() string { return u.msg }
