1295c43c098e4647fcd37e5d20cc89f158baf4dc
perror <15058342792@163.com>
2019-01-10 00:08:18 +0800

    删除过时的代码

--- main.go
+++ main.go
@@ -4,7 +4,4 @@ import "GetAnything-Server/server"
 
 func main() {
 	server.StartServer("", "8080")
-	//log := logger.NewLogger("test", logger2.InfoLevel)
-	//log.Info("fdsafs")
-	//log.Error("fdsa")
 }
