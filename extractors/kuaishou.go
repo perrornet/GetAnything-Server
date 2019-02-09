ad50b2379964e90dfd77fd6e0eb1c82bb4822d68
perror <15058342792@163.com>
2019-02-09 11:43:01 +0800

    [快手]增加标题匹配

--- extractors/kuaishou.go
+++ extractors/kuaishou.go
@@ -15,8 +15,9 @@ type kuaishou struct {
 }
 
 var (
-	kuaishouVideo   = regexp.MustCompile(`src="http://.*?\.mp4"`)
-	kuaishouHeaders = map[string]string{"User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"}
+	kuaishouVideo   = regexp.MustCompile(`type="video/mp4" src=".*?\.mp4"`)
+	kuaishouHeaders = map[string]string{"User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.81 Mobile Safari/537.36"}
+	kuaishouTitle   = regexp.MustCompile(`<meta itemprop=name content=.*?>`)
 )
 
 func (k *kuaishou) Init(url string) error {
@@ -27,16 +28,22 @@ func (k *kuaishou) Init(url string) error {
 func (k *kuaishou) GetDownloadHeaders() map[string]string { return nil }
 func (k *kuaishou) GetFileInfo() ([]download.Info, error) {
 	data := make([]download.Info, 0)
-	resp, err := k.client.Get(k.url.String(), nil)
+	resp, err := k.client.Get(k.url.String(), kuaishouHeaders)
 	if err != nil {
 		return data, err
 	}
+	var title string
 	c, _ := ioutil.ReadAll(resp.Body)
+	titles := kuaishouTitle.FindAll(c, 1)
+	if len(titles) > 0 {
+		title = strings.Replace(string(titles[0]), `<meta itemprop=name content=`, "", 1)
+		title = strings.Replace(title, `>`, "", 1)
+	}
 	for _, t := range kuaishouVideo.FindAllString(string(c), 1) {
 		if t != "" {
-			t = strings.Replace(t, `src="`, "", 1)
+			t = strings.Replace(t, `type="video/mp4" src="`, "", 1)
 			t = strings.Replace(t, `"`, "", 1)
-			data = append(data, download.Info{Url: t})
+			data = append(data, download.Info{Url: t, Title: title, Type: "mp4"})
 			return data, nil
 		}
 	}
