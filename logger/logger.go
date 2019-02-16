d805e928b1f0e321e57b021d5df7b15cabc17074
perror <15058342792@163.com>
2019-02-17 07:37:03 +0800

    使用默认日志结构

--- logger/logger.go
+++ logger/logger.go
@@ -5,6 +5,5 @@ import "github.com/apsdehal/go-logger"
 func NewLogger(name string, level logger.LogLevel) *logger.Logger {
 	log, _ := logger.New(name)
 	log.SetLogLevel(level)
-	log.SetFormat("[%{module}]%{level} %{filename}:%{line}: %{message}")
 	return log
 }
