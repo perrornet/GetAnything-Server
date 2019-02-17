316b0981b0a1b7d56e593f492a5210e2accd2b21
perror <15058342792@163.com>
2019-02-17 08:23:02 +0800

    [bilibili]修复bilibili视频下载错误

--- extractors/bilibili.go
+++ extractors/bilibili.go
@@ -1,10 +1,10 @@
 package extractors
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/PerrorOne/GetAnything-Server/download"
-	"github.com/PerrorOne/GetAnything-Server/utils"
 	"io/ioutil"
 	url2 "net/url"
 	"regexp"
@@ -13,7 +13,7 @@ import (
 
 var (
 	bilibiliCid   = regexp.MustCompile(`","cid":(\d+),`)
-	bilibiliUrl   = regexp.MustCompile(`\<url\>\<!\[CDATA\[(.*?)\]\]`)
+	bilibiliUrl   = regexp.MustCompile(`__playinfo__=(?s:(.*?))<`)
 	bilibiliTitle = regexp.MustCompile(`"title":"(.*?)",`)
 )
 
@@ -23,6 +23,15 @@ type bilibili struct {
 	headers map[string]string
 	SEC1    string
 	ApiUrl  string
+}
+
+type bilibiliResponse struct {
+	Code int `json:"code"`
+	Data struct {
+		Durl []struct {
+			URL string `json:"url"`
+		} `json:"durl"`
+	} `json:"data"`
 }
 
 func (b *bilibili) Init(url string) error {
@@ -44,10 +53,10 @@ func (b *bilibili) GetFileInfo() ([]download.Info, error) {
 			return nil, err
 		}
 		c, _ := ioutil.ReadAll(resp.Body)
-		cids := bilibiliCid.FindAll(c, 1)
-		if len(cids) == 0 {
-			return nil, errors.New("未能再当前页面寻找到cid")
-		}
+		//cids := bilibiliCid.FindAll(c, 1)
+		//if len(cids) == 0 {
+		//	return nil, errors.New("未能再当前页面寻找到cid")
+		//}
 		titles := bilibiliTitle.FindAll(c, 1)
 		var title string
 		if len(titles) > 0 {
@@ -55,29 +64,37 @@ func (b *bilibili) GetFileInfo() ([]download.Info, error) {
 			title = strings.Replace(string(titles[0]), `"title":"`, "", 1)
 			title = strings.Replace(title, `",`, "", 1)
 		}
-		cid := strings.Replace(string(cids[0]), `","cid":`, "", 1)
-		cid = strings.Replace(cid, `,`, "", 1)
-		data1 := []int{116, 112, 80, 74, 64, 32, 16, 15}
-		for _, v := range data1 { // 只选择最高清晰度
-			params := fmt.Sprintf("appkey=84956560bc028eb7&cid=%s&otype=xml&qn=%d&quality=%d&type=", cid, v, v)
-			url := b.ApiUrl + params + "&sign=" + utils.NewMd5(params+b.SEC1).Encrypt()
-			fmt.Println(url)
-			resp, err = b.client.Get(url, nil)
-			if err != nil {
-				return nil, err
-			}
-			c, _ = ioutil.ReadAll(resp.Body)
-			urls := bilibiliUrl.FindAll(c, 1)
-			if len(urls) == 0 {
-				continue
-			}
-			downloadUrl := strings.Replace(string(urls[0]), `<url><![CDATA[`, "", 1)
-			downloadUrl = strings.Replace(downloadUrl, `]]`, "", 1)
-			data := make([]download.Info, 0)
-			data = append(data, download.Info{Title: title, Url: downloadUrl, Type: "flv"})
-			return data, nil
+		data := bilibiliUrl.FindAllSubmatch(c, 1) // from you-get
+		bresp := new(bilibiliResponse)
+		if err := json.Unmarshal(data[0][1], bresp); err != nil {
+			return nil, err
 		}
-		return nil, errors.New("未能找到相关的视频下载URL")
+		if bresp.Code != 0 || len(bresp.Data.Durl) == 0 {
+			return nil, errors.New("bilibili返回数据错误")
+		}
+		return []download.Info{{Title: title, Type: "flv", Url: bresp.Data.Durl[0].URL}}, nil
+		//cid := strings.Replace(string(cids[0]), `","cid":`, "", 1)
+		//cid = strings.Replace(cid, `,`, "", 1)
+		//data1 := []int{116, 112, 80, 74, 64, 32, 16, 15}
+		//for _, v := range data1 { // 只选择最高清晰度
+		//	params := fmt.Sprintf("appkey=84956560bc028eb7&cid=%s&otype=xml&qn=%d&quality=%d&type=", cid, v, v)
+		//	url := b.ApiUrl + params + "&sign=" + utils.NewMd5(params+b.SEC1).Encrypt()
+		//	fmt.Println(url)
+		//	resp, err = b.client.Get(url, nil)
+		//	if err != nil {
+		//		return nil, err
+		//	}
+		//	c, _ = ioutil.ReadAll(resp.Body)
+		//	urls := bilibiliUrl.FindAll(c, 1)
+		//	if len(urls) == 0 {
+		//		continue
+		//	}
+		//	downloadUrl := strings.Replace(string(urls[0]), `<url><![CDATA[`, "", 1)
+		//	downloadUrl = strings.Replace(downloadUrl, `]]`, "", 1)
+		//	data := make([]download.Info, 0)
+		//	data = append(data, download.Info{Title: title, Url: downloadUrl, Type: "flv"})
+		//	return data, nil
+		//}
 	}
 	return nil, nil
 }
