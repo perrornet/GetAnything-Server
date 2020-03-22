0b1f84eb4459222e309d12dd7514beb830688b70
perror <qinyalai@gmail.com>
2020-03-22 12:50:14 +0800

    使用环境变量控制服务监听端口

--- utils/cmd.go
+++ utils/cmd.go
@@ -1,18 +1,33 @@
 package utils
 
-import "flag"
+import (
+	"os"
+	"strings"
+)
 
 type CmdArgs struct {
-	Host *string
-	Port *string
-	Mode *string
+	Host string
+	Port string
+	Mode string
 }
 
 func GetCmd() *CmdArgs {
 	cmd := &CmdArgs{}
-	cmd.Host = flag.String("h", "0.0.0.0", "限制外部访问IP地址（默认：0.0.0.0）")
-	cmd.Port = flag.String("p", "80", "监听端口（默认：80）")
-	cmd.Mode = flag.String("m", "release", "当前服务器状态（默认： release， 可选：debug/release）")
-	flag.Parse()
+	cmd.Host = os.Getenv("HOST")
+	if strings.TrimSpace(cmd.Host) == "" {
+		cmd.Host = "0.0.0.0"
+	}
+	cmd.Port = os.Getenv("PORT")
+	if strings.TrimSpace(cmd.Port) == "" {
+		cmd.Port = "80"
+	}
+	mode := os.Getenv("MODE")
+	switch strings.TrimSpace(mode) {
+	case "debug", "release":
+		cmd.Mode = strings.TrimSpace(mode)
+	default:
+		cmd.Mode = "release"
+
+	}
 	return cmd
 }
